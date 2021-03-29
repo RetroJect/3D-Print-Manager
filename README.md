5e1ac08ee1192bfc5f150b7f18b91ee28687f9c1
Grayson Doshier <grayson.doshier@gmail.com>
2021-03-29 16:43:09 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# 3D Print Manager
+ A Flutter Application for managing your collection of 3D printed items
---
+++ android/app/src/main/kotlin/com/retroject/print_manager_3d/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.retroject.print_manager_3d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
